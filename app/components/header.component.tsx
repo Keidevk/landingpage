956ba0a10b9c5bca70597dcb26c4640cde18c48c
Keidevk <keinnerv11@gmail.com>
2025-05-21 01:18:34 -0400

    bg-image

--- app/components/header.component.tsx
+++ app/components/header.component.tsx
@@ -6,12 +6,11 @@ export default function Header(){
     return(
     <>
     <header>
-        <nav className="grid grid-cols-5 ">
+        <nav className="grid grid-cols-4 ">
             <Image className="rounded-full" alt="logo" width={100} height={100} src={`/logo1.jpg`}/>
-            <Link className="py-10" href={`#`}>Historia de la parroquia</Link>
+            <Link className="py-10" href={`/historia`}>Historia de la parroquia</Link>
             <Link className="py-10" href={`#`}>Organización</Link>
             <Link className="py-10" href={`#`}>Actividades</Link>
-            <Link className="py-10" href={`#`}>Biblia</Link>
         </nav>
     </header>
     </>)
--- app/components/main.component.tsx
+++ app/components/main.component.tsx
@@ -6,13 +6,12 @@ export default function Main(){
     return(
     <>
     <main>
-        <div className="grid grid-cols-3 my-28">
-            <Image className="w-min h-min m-auto" alt="img2" width={300} height={300} src={`/logo2.jpg`}/>
-            <div>
-                <h1>Ubicación</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, eligendi praesentium. Officiis, tempora sint? Optio similique sint saepe? Ipsa tempore velit qui perspiciatis quos. Ut earum facilis minus et hic!</p>
-            </div>
-            <Image className="w-min h-min m-auto" alt="img1" width={300} height={300} src={`/logo3.jpg`}/>
+        <div className="my-10 bg-[#CC0900]">
+            <Image className="w-min h-min m-auto " alt="img2" width={300} height={300} src={`/logo2.jpg`}/>
+        </div>
+        <div>
+            <h1>Ubicación</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, eligendi praesentium. Officiis, tempora sint? Optio similique sint saepe? Ipsa tempore velit qui perspiciatis quos. Ut earum facilis minus et hic!</p>
         </div>
         {/* <Historia/> */}
     </main>
