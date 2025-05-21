d58bbbd01d70f85ec4f32fa17a0a6a2094bba04f
Keidevk <keinnerv11@gmail.com>
2025-05-21 00:45:57 -0400

    Diseno de ubicacion y nav

--- app/components/main.component.tsx
+++ app/components/main.component.tsx
@@ -1,18 +1,20 @@
 'use client'
 import Image from "next/image";
-import { Historia } from "./historia.component";
+// import { Historia } from "./historia.component";
 
 export default function Main(){
     return(
     <>
-    <main className="grid grid-cols-3">
-        <Image alt="img2" width={300} height={300} src={`/logo2.jpg`}/>
-        <div>
-            <h1>Ubicación</h1>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, eligendi praesentium. Officiis, tempora sint? Optio similique sint saepe? Ipsa tempore velit qui perspiciatis quos. Ut earum facilis minus et hic!</p>
+    <main>
+        <div className="grid grid-cols-3 my-28">
+            <Image className="w-min h-min m-auto" alt="img2" width={300} height={300} src={`/logo2.jpg`}/>
+            <div>
+                <h1>Ubicación</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, eligendi praesentium. Officiis, tempora sint? Optio similique sint saepe? Ipsa tempore velit qui perspiciatis quos. Ut earum facilis minus et hic!</p>
+            </div>
+            <Image className="w-min h-min m-auto" alt="img1" width={300} height={300} src={`/logo3.jpg`}/>
         </div>
-        <Image alt="img1" width={300} height={300} src={`/logo3.jpg`}/>
-        <Historia/>
+        {/* <Historia/> */}
     </main>
     </>)
 }
