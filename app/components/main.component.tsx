b7846e77576809ac5e36c9e575f8a91990587626
Keidevk <keinnerv11@gmail.com>
2025-05-21 01:36:47 -0400

    sin comillas y header arreglado

--- app/components/main.component.tsx
+++ app/components/main.component.tsx
@@ -1,32 +1,17 @@
 'use client'
 import Image from "next/image";
-<<<<<<< HEAD
-=======
-// import { Historia } from "./historia.component";
->>>>>>> 956ba0a10b9c5bca70597dcb26c4640cde18c48c
 
 export default function Main(){
     return(
     <>
-<<<<<<< HEAD
-    <main className="">
-        <Image alt="img2" width={300} height={300} src={`/logo2.jpg`}/>
-=======
     <main>
         <div className="my-10 bg-[#CC0900]">
             <Image className="w-min h-min m-auto " alt="img2" width={300} height={300} src={`/logo2.jpg`}/>
         </div>
->>>>>>> 956ba0a10b9c5bca70597dcb26c4640cde18c48c
         <div>
             <h1>Ubicación</h1>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, eligendi praesentium. Officiis, tempora sint? Optio similique sint saepe? Ipsa tempore velit qui perspiciatis quos. Ut earum facilis minus et hic!</p>
         </div>
-<<<<<<< HEAD
-        <Image alt="img1" width={300} height={300} src={`/logo3.jpg`}/>
-        
-=======
-        {/* <Historia/> */}
->>>>>>> 956ba0a10b9c5bca70597dcb26c4640cde18c48c
     </main>
     </>)
 }
